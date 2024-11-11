5fe2ec8406c16b6aba4b4ea62f4ca559450e9029
Justin Wozniak <woz@anl.gov>
2024-11-11 15:39:23 -0600

    Latest versions

--- setup_deps.sh
+++ setup_deps.sh
@@ -6,18 +6,29 @@ set -eu
 # Installs Uno dependencies for these tested cases:
 
 # Python    TensorFlow     Platform
-#    3.8        2.10.0     Linux & Mac M1
-#    3.9        2.13       Linux & Mac M1
+#   3.8         2.10.0     Linux & Mac M1
+#   3.9         2.13       Linux & Mac M1
+#   3.11        2.16       Linux
 
 PV=$( python --version )
 
-if [[ $PV == "Python 3.8"* ]]
+# Package tensorflow-gpu goes up to 2.21
+# Package tensorflow     continues after that
+
+if [[ $PV == "Python 3.8"* ]]  # Up to TF 2.13
 then
-  PROTOBUF="protobuf==3.19.6"
-  TENSORFLOW="tensorflow-gpu==2.10.0"
-else  # 3.9
+  # PROTOBUF="protobuf==3.19.6"
+  # TENSORFLOW="tensorflow-gpu==2.10.0"  # Requires CUDA 11.2, not 12.2
+
+  # PROTOBUF="protobuf==3.20.3"
+  # TENSORFLOW="tensorflow==2.13.0"        # Requires CUDA 11.8, not 12.2
+
   PROTOBUF="protobuf==3.20.3"
-  TENSORFLOW="tensorflow==2.13"
+  TENSORFLOW="tensorflow==2.15.0"        # Requires CUDA 12.2
+
+else  # >= 3.9
+  PROTOBUF="protobuf==3.20.3"
+  TENSORFLOW="tensorflow==2.16.1"
 fi
 
 DEPS=( $PROTOBUF $TENSORFLOW
