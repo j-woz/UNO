cb8ee1b2e2d7248e1d5c167ca74f22ab0b9e4792
Justin Wozniak <woz@anl.gov>
2025-05-19 16:36:24 -0500

    Add error handling

--- uno_infer_improve.py
+++ uno_infer_improve.py
@@ -78,8 +78,15 @@ def run(params: Dict):
         model_dir=params["input_model_dir"]
     )
     # Load the pre-trained model
-    model = load_model(modelpath, compile=False)
-    model.compile(optimizer = "Adam", loss = "mse")
+    # model = load_model(modelpath, compile=False)
+    # model.compile(optimizer = "Adam", loss = "mse")
+    print("loading model: '%s'" % modelpath)
+    try:
+        model = load_model(modelpath)
+    except IOError as e:
+        print("model load failed: " + str(e))
+        exit(1)
+
     # Create data generator for batch predictions
     generator_batch_size = params["generator_batch_size"]
     test_steps = int(np.ceil(len(ts_rsp) / generator_batch_size))
