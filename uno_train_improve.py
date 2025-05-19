9696d55e23f344b72af87ce4e0d312d7631f29ea
Justin Wozniak <woz@anl.gov>
2025-05-19 16:37:18 -0500

    Handle learning rate across APIs

--- uno_train_improve.py
+++ uno_train_improve.py
@@ -24,6 +24,19 @@ import improvelib.utils as frm
 # Import parameters
 from params import app_preproc_params, model_preproc_params, app_train_params, model_train_params
 
+# Compatibility function for setting and accessing learning rate
+def set_learning_rate(optimizer, lr):
+    if hasattr(optimizer, "learning_rate"):  # TensorFlow 2.2 and later
+        optimizer.learning_rate = lr
+    else:  # For TensorFlow versions before 2.2
+        optimizer.lr = lr
+
+def get_learning_rate(optimizer):
+    if hasattr(optimizer, "learning_rate"):  # TensorFlow 2.2 and later
+        return optimizer.learning_rate
+    else:  # For TensorFlow versions before 2.2
+        return optimizer.lr
+
 # Import custom utility functions
 from uno_utils_improve import (
     data_merge_generator, 
@@ -326,14 +339,21 @@ def run(params: Dict):
     steps_per_epoch = int(np.ceil(len(tr_rsp) / batch_size))
     validation_steps = int(np.ceil(len(vl_rsp) / generator_batch_size))
 
-    if tf_minor_version >= 16:
-        learning_rate = model.optimizer.learning_rate
-    else:
-        learning_rate = model.optimizer.lr
+    # if tf_minor_version >= 16:
+    #     learning_rate = model.optimizer.learning_rate
+    # else:
+    #     learning_rate = model.optimizer.lr
+
+    # # Instantiate callbacks
+    # lr_scheduler = LearningRateScheduler(
+    #     lambda epoch: warmup_scheduler(epoch, learning_rate, warmup_epochs, initial_lr, max_lr, warmup_type)
+
+    # Set initial learning rate based on TensorFlow version
+    set_learning_rate(optimizer, initial_lr)
 
     # Instantiate callbacks
     lr_scheduler = LearningRateScheduler(
-        lambda epoch: warmup_scheduler(epoch, learning_rate, warmup_epochs, initial_lr, max_lr, warmup_type)
+        lambda epoch: warmup_scheduler(epoch, get_learning_rate(optimizer), warmup_epochs, initial_lr, max_lr, warmup_type)
     )
 
     reduce_lr = ReduceLROnPlateau(
@@ -365,16 +385,19 @@ def run(params: Dict):
     train_gen = data_merge_generator(tr_rsp, tr_ge, tr_md, batch_size, params, shuffle=True, peek=True)
     val_gen = data_merge_generator(vl_rsp, vl_ge, vl_md, generator_batch_size, params, shuffle=False, peek=True)
 
+    print("steps_per_epoch: %i" % steps_per_epoch)
+    print("validation_steps: %i" % validation_steps)
     # Fit model
     history = model.fit(
         train_gen,
         validation_data=val_gen,
         steps_per_epoch=steps_per_epoch,
-        validation_steps=validation_steps,
+        # validation_steps=validation_steps,
         epochs=epochs,
         callbacks=[r2_callback, lr_scheduler, reduce_lr, early_stopping],
     )
-
+    print("model.fit() done")
+    sys.stdout.flush()
     epoch_end_time = time.time()
     total_epochs = len(history.history['loss'])
     global time_per_epoch 
